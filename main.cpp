5bdec4f8c45e1fea29e63b38edccfc3c8526b0f9
Mike-debug <1781937775@qq.com>
2024-05-04 16:47:32 +0800

    make up project

--- main.cpp
+++ main.cpp
@@ -1,96 +1,22 @@
 #include <cstdlib>
 #include <iostream>
-#include <map>
 #include <string>
 
 #include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/config/asio_client.hpp>
 #include <websocketpp/client.hpp>
+#include <utility>
 
 #include <json/json.h>
 
 #include "infrastructure/webSocket"
 
 #include "CommonUtils/jsonSpecificUtils"
-#include "commonUtils.h"
-
-using namespace std;
-
-pair<double, double> getTimeDelay() {
-    websocket_endpoint endpoint;
-
-    std::string input{"connect wss://testnet.binance.vision/ws-api/v3"};
-
-    int id = endpoint.connect(input.substr(8));
-    if (id != -1) {
-        std::cout << "> Created connection with id " << id << std::endl;
-    }
-
-    int done = 5;
-    while (done > 0) {
-        Sleep(2000);
-        --done;
-        input = "send 0 {\"id\":1,\"method\":\"time\"}\n";
-        std::stringstream ss(input);
-
-        std::string cmd;
-        std::string message;
-
-        ss >> cmd >> id;
-        std::getline(ss, message);
-
-        endpoint.send(id, message);
-    }
-    Sleep(2000);
-    auto msgs = endpoint.get_metadata(id)->getMst();
-    auto msgNum = msgs.size() & (~0x01);
-    vector<unsigned long long> timeStamps{};
-    cout << "msgNum: " << msgNum << endl;
-    // 创建 JSON 对象
-    Json::Value root;
-
-    // 解析 JSON 字符串
-    Json::Reader reader;
-    for (int i{0}; i < msgNum; ++i) {
-        if (!(i & (0x01))) {
-            timeStamps.emplace_back(stoull(msgs.at(i).substr(3, 13)));
-        } else {
-            timeStamps.emplace_back(stoull(msgs.at(i).substr(3, 13)));
-            reader.parse(msgs.at(i).substr(18), root);
-            timeStamps.emplace_back(stoull(root["result"]["serverTime"].asString()));
-        }
-    }
-    printVector(timeStamps);
-
-    double diff{};
-    double delay{};
-    for (int i{0}; i < timeStamps.size(); i += 3) {
-        diff += (timeStamps[i] + timeStamps[i + 1] - 2 * timeStamps[i + 2]) * 0.5;
-        delay += (timeStamps[i + 1] - timeStamps[i]) * 0.5;
-    }
-    diff /= (timeStamps.size() / 3);
-    delay /= (timeStamps.size() / 3);
-
-    auto ret = make_pair(diff, delay);
-    printPair(ret);
-
-    input = "close 0";
-    std::stringstream ss(input);
-
-    std::string cmd;
-    int close_code = websocketpp::close::status::normal;
-    std::string reason;
-
-    ss >> cmd >> id >> close_code;
-    std::getline(ss, reason);
-
-    endpoint.close(id, close_code, reason);
-    Sleep(2000);
-    return ret;
-}
+#include "infrastructure/testNest"
 
 int main() {
-    auto diffDelay = getTimeDelay();
+    using namespace std;
+    auto diffDelay = testnet::getTimeDelay();
     DIFF = diffDelay.first;
 
     bool done = false;
@@ -168,4 +94,3 @@ int main() {
 
     return 0;
 }
-
